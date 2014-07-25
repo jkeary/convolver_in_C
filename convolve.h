8d5c9e512e6068f69bc38ee92e0f199043ff5679
James <jkeary@gmail.com>
2014-07-25 15:46:25 -0400

    first commit

---
+++ convolve.h
@@ -0,0 +1,13 @@
+/*
+** convolve.h
+**
+** Header file for convolve.c
+**
+*/
+
+#ifndef _CONVOLVE_H_
+#define _CONVOLVE_H_
+
+int convolve(float *x, float *h, int lenX, int lenH, float **output);
+
+#endif _CONVOLVE_H_
